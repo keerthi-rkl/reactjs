8b3f84819d8de7c33eeadbffdb2711b2094abfa2
keerthi-rkl <keerthiagustd.p@gmail.com>
2022-01-21 20:07:00 +0530

    commit

---
+++ my-app/src/Form.js
@@ -0,0 +1,16 @@
+const Form=()=>{
+    return (
+        <form>
+          <h4 style={{color:"blue"}}><label >ENTER YOUR NAME:<input type="text"/></label></h4><br></br>
+          <h4 style={{color:"blue"}}><label >ENTER YOUR EMAIL ID:<input type="text"/></label></h4><br></br>
+          <h4 style={{color:"blue"}}><label >ENTER YOUR PASSWORD:<input type="password"/></label></h4><br></br>
+          <input type="submit"/>
+    
+          
+          
+        
+        </form>
+    )
+   
+}
+export default Form;
---
+++ my-app/src/Product.jsx
@@ -0,0 +1,13 @@
+import App from "./App"
+let Product=()=>{
+    let name="redmi";
+    let color="frost white";
+    let cost="17480";
+    return <><h3>product:{name,color,cost}</h3>
+    <hr/>
+    <App/>
+    
+    </>
+
+}
+export default Product;
