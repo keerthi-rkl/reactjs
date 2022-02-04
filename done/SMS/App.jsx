dc48c3c183274cdbbe3b4309aa94290d0f54b283
keerthi-rkl <keerthiagustd.p@gmail.com>
2022-02-04 14:42:10 +0530

    commit

---
+++ done/SMS/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Smsapp from './Smsapp';
+
+class App extends Component {
+  render() {
+    return <div>
+        <Smsapp/>
+    </div>;
+  }
+}
+
+export default App;
